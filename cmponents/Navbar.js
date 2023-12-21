21ac4884168a34b631af82111257fa7a20860ee2
Anshul.poonia <apoonia432@gmail.com>
2023-12-21 22:21:22 +0530

    first commit

--- cmponents/Navbar.js
+++ cmponents/Navbar.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Navbar = () => {
   return (
     <div>
-      <ul className='flex sticky justify-center bg-blue-400'>
+      <ul className='flex sticky justify-center bg-slate-600'>
         <li className='m-2 p-2 font-bold'>Home</li>
         <li className='m-2 p-2 font-bold'>About</li>
         <li className='m-2 p-2 font-bold'>Contact</li>
